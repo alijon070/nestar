a4bc9d70970467a1de7a42209b3449a9a79b537a
Albert <muminovali648@gmail.com>
2026-08-20 23:15:17 +0900

    feat: hashing password via bcryptjs

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -5,12 +5,17 @@ import { Member } from '../../libs/dto/member/member';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class MemberService {
-	constructor(@InjectModel('Member') private readonly memberModel: Model<Member>) {}
+	constructor(
+		@InjectModel('Member') private readonly memberModel: Model<Member>,
+		private authservice: AuthService,
+	) {}
 
 	public async signup(input: MemberInput): Promise<Member> {
+		input.memberPassword = await this.authservice.hashPassword(input.memberPassword);
 		try {
 			const result = await this.memberModel.create(input);
 			return result;
@@ -30,7 +35,11 @@ export class MemberService {
 			throw new InternalServerErrorException(Message.BLOCKED_USER);
 		}
 
-		const isMatch = memberPassword === response.memberPassword;
+		if (!response.memberPassword) {
+			throw new InternalServerErrorException(Message.NO_MEMBER_NICK);
+		}
+
+		const isMatch = await this.authservice.comparePasswords(input.memberPassword, response.memberPassword);
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
 
 		return response;
